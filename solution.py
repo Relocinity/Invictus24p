1a50fb34d12a0b0f3dacaa819c5b08d408a00ae8
Relocinity <158862070+Relocinity@users.noreply.github.com>
2024-02-06 20:31:59 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -80,8 +80,8 @@ class Crypt_Image():
 
 
 class Segment_Image():
-    def __init__(self,img,bv,gv,rv):
-        self.orig = img
+    def __init__(self,src,bv,gv,rv):
+        self.orig = src
         self.img = self.orig
         self.matSize = self.img.shape[:2]
         self.b,self.g,self.r = cv2.split(self.orig)
